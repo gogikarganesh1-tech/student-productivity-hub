208575bd771a1f6d11a7f12573c749262faf62ee
gogikarganesh1-tech <gogikarganesh1@gmail.com>
2026-06-09 15:53:31 +0530

    Add timer functionality to script.js

--- script.js
+++ script.js
@@ -28,3 +28,31 @@ function checkAttendance(){
     .innerHTML =
     "Attendance: " + percentage.toFixed(2) + "%";
 }
+let time = 1500;
+
+function startTimer(){
+
+    let interval =
+    setInterval(function(){
+
+        let minutes =
+        Math.floor(time/60);
+
+        let seconds =
+        time%60;
+
+        document.getElementById("timer")
+        .innerHTML =
+        minutes + ":" +
+        (seconds<10?"0":"") +
+        seconds;
+
+        time--;
+
+        if(time<0){
+            clearInterval(interval);
+            alert("Study Session Complete!");
+        }
+
+    },1000);
+}
